0af072d1c275f3ff7a5c1bd083ee0befcdc07a59
t.miyazu <myztmm@gmail.com>
2018-03-30 18:53:47 +0900

    Add simple application class

---
+++ src/main/scala/moe/awawa/taiban/TaibanApp.scala
@@ -0,0 +1,9 @@
+package moe.awawa.taiban
+
+import org.slf4j.LoggerFactory
+
+object TaibanApp extends App {
+  val logger = LoggerFactory.getLogger(this.getClass)
+  println("Hello!")
+  logger.info("World!")
+}
